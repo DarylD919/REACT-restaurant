62b186d7bd086cda8f7452a9530030f84e005daa
darilag2 <daryl_darilag@yahoo.co.uk>
2021-01-21 12:20:02 +0000

    get page complete

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import image1 from '../images/img-10.jpg'
 import image2 from '../images/img-11.jpg'
 import image3 from '../images/img-12.jpg'
@@ -11,70 +10,64 @@ export default function Card() {
     return (
         <div className="selection">
             <div className="card">
-                <Link to="/get" className="card-link">
                     <div className="card-container">
                         <img src={image1} alt="bake" className="img-src" />
-                        <div className="card-title"><h3>Mc Donald's</h3></div>
+                        <div className="card-title"><h3>Sample</h3></div>
                         <div className="card-content">
                             <p>Fast Food Chain Restaurant</p>
-                        </div>
+                            <p><button className="get-btn">Add to cart</button></p>
                     </div>
-                </Link>            
+                </div>            
             </div>  
             <div className="card">
-                <Link to="/get" className="card-link">
-                    <div className="card-container">
-                        <img src={image2} alt="bake" className="img-src" />
-                        <div className="card-title"><h3>Mc Donald's</h3></div>
-                        <div className="card-content">
-                            <p>Fast Food Chain Restaurant</p>
+                <div className="card-container">
+                    <img src={image2} alt="bake" className="img-src" />
+                    <div className="card-title"><h3>Sample</h3></div>
+                    <div className="card-content">
+                        <p>Fast Food Chain Restaurant</p>
+                        <p><button className="get-btn">Add to cart</button></p>
                         </div>
-                    </div>
-                </Link>            
+                </div>            
             </div>  
             <div className="card">
-                <Link to="/get" className="card-link">
-                    <div className="card-container">
-                        <img src={image3} alt="bake" className="img-src" />
-                        <div className="card-title"><h3>Mc Donald's</h3></div>
-                        <div className="card-content">
-                            <p>Fast Food Chain Restaurant</p>
-                        </div>
+              <div className="card-container">
+                    <img src={image3} alt="bake" className="img-src" />
+                    <div className="card-title"><h3>Sample</h3></div>
+                    <div className="card-content">
+                        <p>Fast Food Chain Restaurant</p>
+                        <p><button className="get-btn">Add to cart</button></p>
                     </div>
-                </Link>            
+                </div>            
             </div>  
             <div className="card">
-                <Link to="/get" className="card-link">
-                    <div className="card-container">
-                        <img src={image4} alt="bake" className="img-src" />
-                        <div className="card-title"><h3>Mc Donald's</h3></div>
-                        <div className="card-content">
-                            <p>Fast Food Chain Restaurant</p>
-                        </div>
+                <div className="card-container">
+                    <img src={image4} alt="bake" className="img-src" />
+                    <div className="card-title"><h3>Sample</h3></div>
+                    <div className="card-content">
+                        <p>Fast Food Chain Restaurant</p>
+                        <p><button className="get-btn">Add to cart</button></p>
                     </div>
-                </Link>            
+               </div>
             </div>  
             <div className="card">
-                <Link to="/get" className="card-link">
-                    <div className="card-container">
-                        <img src={image5} alt="bake" className="img-src" />
-                        <div className="card-title"><h3>Mc Donald's</h3></div>
-                        <div className="card-content">
-                            <p>Fast Food Chain Restaurant</p>
-                        </div>
+                <div className="card-container">
+                    <img src={image5} alt="bake" className="img-src" />
+                    <div className="card-title"><h3>Sample</h3></div>
+                    <div className="card-content">
+                        <p>Fast Food Chain Restaurant</p>
+                        <p><button className="get-btn">Add to cart</button></p>
                     </div>
-                </Link>            
+                </div>
             </div>  
             <div className="card">
-                <Link to="/get" className="card-link">
-                    <div className="card-container">
-                        <img src={image6} alt="bake" className="img-src" />
-                        <div className="card-title"><h3>Mc Donald's</h3></div>
-                        <div className="card-content">
-                            <p>Fast Food Chain Restaurant</p>
-                        </div>
+                <div className="card-container">
+                    <img src={image6} alt="bake" className="img-src" />
+                    <div className="card-title"><h3>Sample</h3></div>
+                    <div className="card-content">
+                        <p>Fast Food Chain Restaurant</p>
+                        <p><button className="get-btn">Add to cart</button></p>
                     </div>
-                </Link>            
+                </div>
             </div>  
         </div>
     )
